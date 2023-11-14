d96339ed4640a640ca71b4aafda06ae94d4e3fb9
mahmoudalame <alamemany27@gmail.com>
2023-11-14 23:52:28 +0100

    Initial commit

---
+++ app/src/main/java/com/many/andriodpraxis1/MainActivity.kt
@@ -0,0 +1,13 @@
+
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.layout3)
+        //setContentView(R.layout.layout2)
+        //setContentView(R.layout.layout3)
+    }
+}
