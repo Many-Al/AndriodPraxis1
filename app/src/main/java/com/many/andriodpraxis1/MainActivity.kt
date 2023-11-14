d8e481fc21cb5ba171395c54c2c8105e7b4a04e9
mahmoudalame <alamemany27@gmail.com>
2023-11-14 23:58:21 +0100

    main geändert

--- app/src/main/java/com/many/andriodpraxis1/MainActivity.kt
+++ app/src/main/java/com/many/andriodpraxis1/MainActivity.kt
@@ -1,4 +1,4 @@
-
+package com.many.andriodpraxis1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -6,8 +6,6 @@ import android.os.Bundle
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.layout3)
-        //setContentView(R.layout.layout2)
-        //setContentView(R.layout.layout3)
+        setContentView(R.layout.activity_main)
     }
 }
